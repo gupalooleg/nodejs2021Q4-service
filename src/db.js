5551b3b30e59a927055e7a2aa1a658859dda2f09
Oleg Gupalo <gupalooleg@gmail.com>
2021-12-04 17:46:56 +0300

    fix: change db schema

--- src/db.js
+++ src/db.js
@@ -1,17 +1,27 @@
-module.exports = {
-  Board: [],
-  Column: [],
-  Task: [],
-  User: [],
-  Constraints: {
-    fkBoardTaskOnDeleteCascade: (id) => {
-      this.Task = this.Task.filter((value) => value.id !== id);
-    },
-    fkUserTaskOnDeleteSetNull: (id) =>
-      this.Task.forEach((value, index, array) => {
-        if (value.id === id) {
-          array[index].id = null;
-        }
-      }),
+const Board = [];
+const Column = [];
+const Task = [];
+const User = [];
+const Constraints = {
+  fkConstrBoardTaskOnDeleteCascade: (boardId) => {
+    Task.splice(
+      0,
+      Task.length,
+      ...Task.filter((value) => value.boardId !== boardId)
+    );
   },
+  fkConstrUserTaskOnDeleteSetNull: (userId) =>
+    Task.forEach((value, index, array) => {
+      if (value.userId === userId) {
+        array[index].userId = null;
+      }
+    }),
+};
+
+module.exports = {
+  Board,
+  Column,
+  Task,
+  User,
+  ...Constraints,
 };
